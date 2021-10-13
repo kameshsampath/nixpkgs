801f8f9f13efa6599c8a68712f4383790febc3fc
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-10-13 14:21:01 +0530

    export custom packages

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
   description = "My custom nix package collection";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
 
-  outputs = { self, nixpkgs, flake-utils }: {
-    packages.x86_64-linux.kameshsampath =
-      (import ./pkgs) { inherit (self) pkgs; };
-    packages.x86_64-darwin.kameshsampath =
-      (import ./pkgs) { inherit (self) pkgs; };
-  };
+  outputs = { self, nixpkgs }:
+
+    {
+      packages.x86_64-linux =
+        import ./pkgs { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
+      packages.x86_64-darwin =
+        import ./pkgs { pkgs = nixpkgs.legacyPackages.x86_64-darwin; };
+    };
 }
